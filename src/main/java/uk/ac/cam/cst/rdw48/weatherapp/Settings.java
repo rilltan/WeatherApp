3187d640735d01539f0a6b7a64e352304fe7bea1
rohan <rohandwadhawan@gmail.com>
2025-05-15 15:37:58 +0100

    add settings and forecast page

---
+++ src/main/java/uk/ac/cam/cst/rdw48/weatherapp/Settings.java
@@ -0,0 +1,16 @@
+package uk.ac.cam.cst.rdw48.weatherapp;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Settings extends Application {
+
+  @Override
+  public void start(Stage stage) {
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
